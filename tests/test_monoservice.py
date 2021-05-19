3fce3ca3b2b83915e49bb9e310a0ad6cf175e9f4
Anis Campos <anis@lumapps.com>
2021-05-19 16:31:59 +0200

    test(git): add coverage
    testing that everything works
    Also disabling the auto coverage, it prevents debugging.

---
+++ tests/test_monoservice.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from git import Repo
+
+from changelog_generator.repository_manager import RepositoryManager
+
+
+def test_get_organization_changelog(organization_repo: Repo):
+    path = str(Path(organization_repo.git_dir).parent)
+    repository = RepositoryManager(path)
+    current_tag = repository.current_tag
+    assert current_tag
+    previous_tag = repository.previous_tag
+    assert previous_tag
+
+    commits = repository.commits_since_last_tag
+    assert commits
