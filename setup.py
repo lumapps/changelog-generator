59e0096872ad47e1d4cc3a47d05bdfdeab497221
Nicolas Noirbent <nicolas.noirbent@lumapps.com>
2025-01-13 10:46:09 +0100

    chore(python): migrate to 3.12

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     maintainer="LumApps core team",
     maintainer_email="core@lumapps.com",
     url="https://github.com/lumapps/changelog-generator",
-    python_requires="~=3.11",
+    python_requires="~=3.12",
     setup_requires=[],
     install_requires=["gitpython", "jinja2"],
     extras_require={},
